1f5954fcb82760bb264f71f1151cd57e566cf969
wolfog <a465273800@gmail.com>
2018-08-24 15:30:12 +0800

    input randomly

--- Task3.py
+++ Task3.py
@@ -64,4 +64,6 @@ if phone_called_list.count("080") != 0 and number_080_call != 0:
     print("%.2f percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore." %
           (phone_called_list.count("080") / number_080_call))
 		  
+放假按法律
+		  
 
